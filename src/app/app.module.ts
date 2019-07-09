96fcd89c7d39ab45e1561811322dd10155801934
ELSACTE PALMA,OCTAVIO <oelsacte@gmail.com>
2019-07-09 00:44:33 -0500

    Se agregan caracteristicas hasta clase 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TimerService } from './timer/timer.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input, Output, OnInit} from '@angular/core';
+import { Component, Input, Output, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-display',
   templateUrl: 'display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent{
+export class DisplayComponent implements OnChanges {
 
-  @Input() time:number = null;
+  @Input() time: number = null;
+  public minutes = '00';
+  public seconds = '00';
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes) {
+      const minutes = Math.trunc(changes.time.currentValue / 60 );
+      const seconds = changes.time.currentValue - minutes * 60;
+      this.minutes = ('0' + minutes).substr(-2);
+      this.seconds = ('0' + seconds).substr(-2);
+    }
+  }
 
 }
